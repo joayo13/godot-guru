209bb278a8daaeca4628219446f2478818059bb2
Jordan Ayotte <jordanayotte13@outlook.com>
2024-05-27 02:03:17 -0400

    added mobile menu component

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,104 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { fade, slide } from 'svelte/transition'
+	import { quintOut } from 'svelte/easing';
+	let isMobile = false;
+	let menuOpen = false;
+
+	function toggleMenu() {
+		menuOpen = !menuOpen;
+	}
+
+	function handleKeydown(event: KeyboardEvent) {
+		if (event.key === 'Escape' && menuOpen) {
+			menuOpen = false;
+		}
+	}
+
+	function handleOverlayClick() {
+		menuOpen = false;
+	}
+	function checkMobile() {
+		isMobile = window.matchMedia('(max-width: 768px)').matches;
+	}
+
+	onMount(() => {
+		checkMobile();
+		window.addEventListener('resize', checkMobile);
+		//Listen for escape key when if mobile menu is open
+		if (typeof document !== 'undefined') {
+			if (menuOpen) {
+				document.body.style.overflow = 'hidden';
+				document.addEventListener('keydown', handleKeydown);
+			} else {
+				document.body.style.overflow = 'auto';
+				document.removeEventListener('keydown', handleKeydown);
+			}
+		}
+
+		return () => {
+			window.removeEventListener('resize', checkMobile);
+		};
+	});
+</script>
+
+<nav>
+	{#if isMobile}
+		<button on:click={toggleMenu}>Hamburger</button>
+	{:else}
+		<a href="/">Home</a>
+		<a href="/about">About</a>
+		<a href="/settings">Settings</a>
+	{/if}
+	<p>{`isMobile: ${isMobile}`}</p>
+	{#if menuOpen}
+		<div
+			class='overlay'
+			on:click={handleOverlayClick}
+			role="presentation"
+			transition:fade={{ delay: 250, duration: 300, easing: quintOut}}
+		></div>
+		<nav class='menu' aria-hidden={!menuOpen} transition:slide={{ delay: 250, duration: 300, easing: quintOut, axis: 'x' }}>
+			<a href="#home">Home</a>
+			<a href="#about">About</a>
+			<a href="#services">Services</a>
+		</nav>
+	{/if}
+	
+</nav>
+
+<slot></slot>
+
+<style>
+	.menu {
+		position: fixed;
+		top: 0;
+		left: 0;
+		height: 100%;
+		width: 250px;
+		background: #333;
+		color: white;
+		overflow-y: auto;
+	}
+
+	.menu a {
+		display: block;
+		padding: 1em;
+		color: white;
+		text-decoration: none;
+	}
+
+	.menu a:hover {
+		background: #444;
+	}
+
+	.overlay {
+		display: block;
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background: rgba(0, 0, 0, 0.5);
+	}
+</style>
